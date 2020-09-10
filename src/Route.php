bd54b6a3000b6b1d5686973b77c75a2bba929e45
MiladM <miladmohebnia@gmail.com>
2020-09-10 14:42:21 +0430

    static call for router

--- src/Route.php
+++ src/Route.php
@@ -2,9 +2,47 @@
 
 namespace miladm;
 
+use miladm\router\Router;
+
 class Route
 {
+    static $router = null;
+
+    public static function use($callback)
+    {
+        $router = self::router();
+        return $router->globalMiddleware_add($callback);
+    }
+
     public static function get($route, $callback)
     {
+        $router = self::router();
+        return $router->get($route, $callback);
+    }
+
+    public static function post($route, $callback)
+    {
+        $router = self::router();
+        return $router->post($route, $callback);
+    }
+
+    public static function any($route, $callback)
+    {
+        $router = self::router();
+        return $router->any($route, $callback);
+    }
+
+    public static function interceptor($callback)
+    {
+        $router = self::router();
+        return $router->globalInterceptor_add($callback);
+    }
+
+    private static function router(): Router
+    {
+        if (self::$router === null) {
+            self::$router = new Router();
+        }
+        return self::$router;
     }
 }
