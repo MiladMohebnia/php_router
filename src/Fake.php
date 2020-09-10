da46f8b866fad88c6c22dcd523e7d4a8f6ce90bc
MiladM <miladmohebnia@gmail.com>
2020-09-10 14:47:45 +0430

    faker

--- src/Fake.php
+++ src/Fake.php
@@ -2,10 +2,30 @@
 
 namespace miladm;
 
+use miladm\faker\Faker;
+
 class Fake
 {
-    static function init()
+
+    static $faker = null;
+
+    static function get($path, $data = [])
     {
-        echo 'this is working';
+        $faker = self::faker($path);
+        return $faker->get($data);
+    }
+
+    static function post($path, $data = [])
+    {
+        $faker = self::faker($path);
+        return $faker->post($data);
+    }
+
+    private static function faker($path): Faker
+    {
+        if (self::$faker === null) {
+            self::$faker = new Faker($path);
+        }
+        return self::$faker;
     }
 }
--- src/faker/Faker.php
+++ src/faker/Faker.php
@@ -2,21 +2,31 @@
 
 namespace miladm\faker;
 
+defined('DEVMODE') ?: define('DEVMODE', false);
+
 class Faker
 {
 
     function __construct($URI)
     {
-        $_SERVER['REQUEST_URI'] = $URI;
-        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-        $_SERVER['HTTP_USER_AGENT'] = 'CLI';
-        if (isset($_SESSION['__COOKIE'])) {
-            $_COOKIE = $_SESSION['__COOKIE'];
+        if ($this->checkIfCLI()) {
+            if ($URI[0] != "/") {
+                $URI = '/' . $URI;
+            }
+            $_SERVER['REQUEST_URI'] = $URI;
+            $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+            $_SERVER['HTTP_USER_AGENT'] = 'CLI';
+            if (isset($_SESSION['__COOKIE'])) {
+                $_COOKIE = $_SESSION['__COOKIE'];
+            }
         }
     }
 
     public function post($data = [])
     {
+        if (!$this->checkIfCLI()) {
+            return $this;
+        }
         $_SERVER['REQUEST_METHOD'] = 'POST';
         $_POST = $data;
         return $this;
@@ -24,6 +34,9 @@ class Faker
 
     public function get($data = [])
     {
+        if (!$this->checkIfCLI()) {
+            return $this;
+        }
         $_SERVER['REQUEST_METHOD'] = 'GET';
         $_GET = $data;
         return $this;
@@ -31,7 +44,15 @@ class Faker
 
     public function add_cookie($data = [])
     {
+        if (!$this->checkIfCLI()) {
+            return $this;
+        }
         $_COOKIE += $data;
         return $this;
+    }
+
+    private function checkIfCLI()
+    {
+        return (DEVMODE && php_sapi_name() === 'cli');
     }
 }
